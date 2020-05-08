a8f6dd90b65100b2d9bfc06439beeaabee45afe3
w3bdesign <hijack_xxx@yahoo.com>
2020-05-09 00:29:30 +0200

    Playing around

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,4 @@
 export default (req, res) => {
-    res.status(200).json({ text: 'Hello' })
+    res.status(200).json([{ text: 'Hello' }])
   }
   
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
-//const axios = require('axios').default;
 import fetch from 'node-fetch';
 
 const url = 'http://localhost:3000';
 
-function HomePage(props) {
+function HomePage({posts}) {
+    //const myArray = Object.entries(posts);
   console.log('Props from Homepage: ');
-  console.log(props);
-  return <div>Welcome to Next.js! Reply from </div>;
+  console.log(posts);
+  return (
+<div>
+    Test
+    <br/>
+
+    {
+    //console.log(Object.entries(posts))
+    posts.map(({
+        text
+      }) => <div key="1">Data fra data: {text} </div>)
+    //props.posts.map(data => <div>{data}</div>)
+    }
+
+</div>
+
+   );
+
+
+  
 }
 
 export async function getStaticProps(context) {
