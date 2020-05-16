511ad30141365ad2d931a1594853c38743893d80
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-17 01:00:02 +0200

    Move nextConfig

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Header from 'components/Header/Header.component';
 import Hero from 'components/Main/Hero.component';
 import IndexProducts from 'components/Main/IndexProducts.component';
 
-import WOO_CONFIG from '../nextConfig';
+import WOO_CONFIG from 'config/nextConfig';
 import { FETCH_ALL_PRODUCTS_QUERY } from 'const/GQL_QUERIES';
 
 // https://apppresser.com/woocommerce-rest-api/
@@ -21,13 +21,11 @@ function HomePage() {
     <>
       <Header />
       <Hero />
-
       {data ? (
         <IndexProducts products={data} />
       ) : (
         <div className="mt-8 text-2xl text-center">Laster produkter ...</div>
       )}
-
       {/* Display error message if error occured */}
       {error && (
         <div className="mt-8 text-2xl text-center">
