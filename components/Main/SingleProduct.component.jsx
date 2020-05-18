036b066666b13300b7d5b6a7cbe243988466affe
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-18 02:25:02 +0200

    We can now fetch single products
    Still need a bit more work, but it seems to work at least.

--- components/Main/SingleProduct.component.jsx
+++ components/Main/SingleProduct.component.jsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
 
-import {withRouter} from "next/router"
+function SingleProduct(props) {
+  const price = 0;
 
+  const {
+    description,
+    image,
+    name,
+    onSale,
+    productId,
+  } = props.product.products.edges[0].node;
 
-function IndexProducts(props) {
- 
   return (
     <section className="py-8 bg-white">
       <div className="container flex flex-wrap items-center pt-4 pb-12 mx-auto">
@@ -36,58 +42,38 @@ function IndexProducts(props) {
             </div>
           </div>
         </nav>
-
-        {props.products.products.nodes.length ? (
-          props.products.products.nodes.map(
-            ({ id, name, price, onSale, slug, image }) => (
-              <div
-                key={uuidv4()}
-                className="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4"
-              >
-                <Link
-                  href={{
-                    pathname: '/produkt',
-                    query: { slug: slug },
-                  }}
-                >
-                  <a>
-                    <img
-                      id="product-image"
-                      className="transition duration-500 ease-in-out transform hover:grow hover:shadow-lg hover:scale-105"
-                      src={image.sourceUrl}
-                    />
-                  </a>
-                </Link>
-                <div className="flex justify-center pt-3">
-                  <p className="font-bold text-center">{name}</p>
-                </div>
-                {/* Display sale price when on sale */}
-
-                {onSale && (
-                  <>
-                    <div className="flex justify-center">
-                      <div className="pt-1 text-gray-900 line-through">
-                        {price}
-                      </div>
-                      <div className="pt-1 ml-2 text-gray-900"> {price}</div>
-                    </div>
-                  </>
-                )}
-                {/* Display regular price when not on sale */}
-                {!onSale && (
-                  <p className="pt-1 text-center text-gray-900"> {price}</p>
-                )}
-              </div>
-            )
-          )
-        ) : (
-          <div className="mx-auto text-xl font-bold text-center text-gray-800 no-underline uppercase">
-            Ingen produkter funnet
+        <div className="flex flex-col w-full p-6 md:w-1/3 xl:w-1/4">
+          <img
+            id="product-image"
+            className="transition duration-500 ease-in-out transform hover:grow hover:shadow-lg hover:scale-105"
+            src={image.sourceUrl}
+          />
+          <div className="flex justify-center pt-3">
+            <p className="font-bold text-center">{name}</p>
           </div>
-        )}
+          {/* Display sale price when on sale */}
+          {onSale && (
+            <>
+              <div className="flex justify-center">
+                <div className="pt-1 text-gray-900 line-through">{price}</div>
+                <div className="pt-1 ml-2 text-gray-900"> {price}</div>
+              </div>
+            </>
+          )}
+          {/* Display regular price when not on sale */}
+          {!onSale && (
+            <p className="pt-1 text-center text-gray-900"> {price}</p>
+          )}
+        </div>
       </div>
     </section>
   );
 }
 
-export default withRouter(IndexProducts);
+export async function getStaticProps(context) {
+  return {
+    // props: { query: context.router.query }, // will be passed to the page component as props
+  };
+}
+
+export default SingleProduct;
