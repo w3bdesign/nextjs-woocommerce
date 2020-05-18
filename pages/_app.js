c22597d451e6d4273e5430849247fde910920ad8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-18 02:46:50 +0200

    Setup React Context for storing cart state

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/index.css'
+import '../styles/index.css';
+
+// Setup localStorage here
+const CartContext = React.createContext();
 
 // Will be called once for every metric that has to be reported.
 export function reportWebVitals(metric) {
@@ -7,7 +10,11 @@ export function reportWebVitals(metric) {
 }
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartContext.Provider value="">
+      <Component {...pageProps} />;
+    </CartContext.Provider>
+  );
 }
 
 export default MyApp;
