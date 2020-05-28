8f0ac7a23a1da6687d4cc20914bdbee01e280fd1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-05-29 01:18:06 +0200

    Add functions.js

--- components/Cart/AddToCartButton.jsx
+++ components/Cart/AddToCartButton.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from 'react';
 
 import { AppContext } from 'utils/context/AppContext';
+import { addFirstProduct } from 'utils/functions/functions';
 
 const AddToCartButton = (props) => {
   console.log('Add to cart button: ');
@@ -20,14 +21,24 @@ const AddToCartButton = (props) => {
     salePrice,
   } = props.product.products.edges[0].node;
 
-  const handleAddToCartClick = () => {
+  const handleAddToCartClick = (props) => {
+
     // If component is rendered on the client side
     if (process.browser) {
+      console.log('Is browser!');
+      console.log(props);
+
       let existingCart = localStorage.getItem('woocommerce-cart');
+      console.log('Existing cart');
+      console.log(existingCart);
       if (existingCart) {
+        console.log('Existing cart status:');
+        console.log(cart);
       } else {
-        //const NewCart = addFirstProduct(props)
-        setCart(product)
+        const NewCart = addFirstProduct(props);
+        setCart('testing cart');
+        console.log('Cart status:');
+        console.log(cart);
       }
     }
   };
@@ -35,7 +46,7 @@ const AddToCartButton = (props) => {
   return (
     <>
       <button
-        onClick={handleAddToCartClick()}
+        onClick={() => handleAddToCartClick()}
         className="px-4 py-2 font-bold bg-white border border-gray-400 border-solid rounded hover:bg-gray-400"
       >
         KJØP
