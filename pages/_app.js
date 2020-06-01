09cacc144d8cb84637bf2902ca189a5ff0eba98d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-02 00:02:00 +0200

    Show LoadingSpinner
    And hide content to prevent FOUC (Flash of unstyled content)

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,34 @@
+import { useState, useEffect } from 'react';
+
 import { AppProvider } from 'utils/context/AppContext';
 
 import Header from '../components/Header/Header.component';
 import Footer from '../components/Footer/Footer.component';
 
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner.component';
+
 import '../styles/index.css';
 import '../styles/algolia.min.css';
 
 const MyApp = ({ Component, pageProps }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
   return (
-    <AppProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </AppProvider>
+    <>
+      {/*Show LoadingSpinner and hide content to prevent FOUC (Flash of unstyled content)*/}
+      {loading && <LoadingSpinner />}
+      {!loading && (
+        <AppProvider>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppProvider>
+      )}
+    </>
   );
 };
 
