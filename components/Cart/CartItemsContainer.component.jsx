c9bb8b78f0978cc2437dd2299d03afd44aa9fbc7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-06 23:46:59 +0200

    Add more functions

--- components/Cart/CartItemsContainer.component.jsx
+++ components/Cart/CartItemsContainer.component.jsx
@@ -33,10 +33,7 @@ const CartItemsContainer = () => {
   // TODO We will focus on fetching the cart before we add more functionality
 
   return (
-    
-
-
-	  <section className="py-8 bg-white">
+    <section className="py-8 bg-white">
       <div className="container flex flex-wrap items-center pt-4 pb-12 mx-auto">
         <nav id="store" className="top-0 w-full px-6 py-1">
           <div className="container flex flex-wrap items-center justify-between w-full px-2 py-3 mx-auto mt-0">
@@ -44,16 +41,10 @@ const CartItemsContainer = () => {
               className="mt-6 text-xl font-bold tracking-wide text-gray-800 no-underline uppercase hover:no-underline"
               href="#"
             >
-             Handlekurv
+              Handlekurv
             </a>
           </div>
         </nav>
-
-
-
-
-
-
 
         {cart ? (
           <p>Vi har innhold i handlekurven!</p>
--- utils/functions/functions.js
+++ utils/functions/functions.js
@@ -72,21 +72,51 @@ export const updateCart = (
 };
 
 /**
- * 
- * @param {Object} existingProductInCart 
- * @param {Object} product 
- * @param {Number} quantityToBeAdded 
- * @param {Number} newQuantity 
+ * Get updated products array
+ * Update the product if it exists else,
+ * add the new product to existing cart,
+ *
+ * @param {Object} existingProductsInCart Existing product in cart
+ * @param {Object} product Product
+ * @param {Integer} qtyToBeAdded Quantity
+ * @param {Integer} newQty New qty of the product (optional)
+ * @return {*[]}
  */
-export const getUpdatedProducts = (existingProductsInCart, product, quantityToBeAdded, newQuantity = false  ) => {
+export const getUpdatedProducts = (
+  existingProductsInCart,
+  product,
+  qtyToBeAdded,
+  newQty = false
+) => {
+  // Check if the product already exits in the cart.
+  const productExitsIndex = isProductInCart(
+    existingProductsInCart,
+    product.productId
+  );
 
-  const productExistsIndex = isProductInCart (existingProductsInCart, product.productID)
-  console.log("productExistsIndex")
-  console.log(productExistsIndex)
+  // If product exits ( index of that product found in the array ), update the product quantity and totalPrice
+  if (-1 < productExitsIndex) {
+    let updatedProducts = existingProductsInCart;
+    let updatedProduct = updatedProducts[productExitsIndex];
 
-  // https://youtu.be/J00IAIbMg2M?list=PLD8nQCAhR3tQ5GvL6zkPb-ASbvgTiCiUb&t=491
+    // If have new qty of the product available, set that else add the qtyToBeAdded
+    updatedProduct.qty = newQty
+      ? parseInt(newQty)
+      : parseInt(updatedProduct.qty + qtyToBeAdded);
+    updatedProduct.totalPrice = parseFloat(
+      (updatedProduct.price * updatedProduct.qty).toFixed(2)
+    );
+
+    return updatedProducts;
+  } else {
+    // If product not found push the new product to the existing product array.
+    let productPrice = getFloatVal(product.price);
+    const newProduct = createNewProduct(product, productPrice, qtyToBeAdded);
+    existingProductsInCart.push(newProduct);
+
+    return existingProductsInCart;
+  }
 };
-
 
 /**
  * Returns index of the product if it exists.
@@ -95,18 +125,17 @@ export const getUpdatedProducts = (existingProductsInCart, product, quantityToBe
  * @param {Integer} productId Product id.
  * @return {number | *} Index Returns -1 if product does not exist in the array, index number otherwise
  */
-const isProductInCart = ( existingProductsInCart, productId ) => {
+const isProductInCart = (existingProductsInCart, productId) => {
+  const returnItemThatExits = (item, index) => {
+    if (productId === item.productId) {
+      return item;
+    }
+  };
 
-	const returnItemThatExits = ( item, index ) => {
-		if ( productId === item.productId ) {
-			return item;
-		}
-	};
+  // This new array will only contain the product which is matched.
+  const newArray = existingProductsInCart.filter(returnItemThatExits);
 
-	// This new array will only contain the product which is matched.
-	const newArray = existingProductsInCart.filter( returnItemThatExits );
-
-	return existingProductsInCart.indexOf( newArray[0] );
+  return existingProductsInCart.indexOf(newArray[0]);
 };
 
 /**
@@ -115,129 +144,124 @@ const isProductInCart = ( existingProductsInCart, productId ) => {
  * @param {Integer} productId Product Id.
  * @return {any | string} Updated cart
  */
-export const removeItemFromCart = ( productId ) => {
+export const removeItemFromCart = (productId) => {
+  let existingCart = localStorage.getItem('woo-next-cart');
+  existingCart = JSON.parse(existingCart);
 
-	let existingCart = localStorage.getItem( 'woo-next-cart' );
-	existingCart = JSON.parse( existingCart );
+  // If there is only one item in the cart, delete the cart.
+  if (1 === existingCart.products.length) {
+    localStorage.removeItem('woo-next-cart');
+    return null;
+  }
 
-	// If there is only one item in the cart, delete the cart.
-	if ( 1 === existingCart.products.length ) {
+  // Check if the product already exits in the cart.
+  const productExitsIndex = isProductInCart(existingCart.products, productId);
 
-		localStorage.removeItem( 'woo-next-cart' );
-		return null;
+  // If product to be removed exits
+  if (-1 < productExitsIndex) {
+    const productTobeRemoved = existingCart.products[productExitsIndex];
+    const qtyToBeRemovedFromTotal = productTobeRemoved.qty;
+    const priceToBeDeductedFromTotal = productTobeRemoved.totalPrice;
 
-	}
+    // Remove that product from the array and update the total price and total quantity of the cart
+    let updatedCart = existingCart;
+    updatedCart.products.splice(productExitsIndex, 1);
+    updatedCart.totalProductsCount =
+      updatedCart.totalProductsCount - qtyToBeRemovedFromTotal;
+    updatedCart.totalProductsPrice =
+      updatedCart.totalProductsPrice - priceToBeDeductedFromTotal;
 
-	// Check if the product already exits in the cart.
-	const productExitsIndex = isProductInCart( existingCart.products, productId );
-
-	// If product to be removed exits
-	if ( -1 < productExitsIndex ) {
-
-		const productTobeRemoved = existingCart.products[ productExitsIndex ];
-		const qtyToBeRemovedFromTotal = productTobeRemoved.qty;
-		const priceToBeDeductedFromTotal = productTobeRemoved.totalPrice;
-
-		// Remove that product from the array and update the total price and total quantity of the cart
-		let updatedCart = existingCart;
-		updatedCart.products.splice( productExitsIndex, 1 );
-		updatedCart.totalProductsCount = updatedCart.totalProductsCount - qtyToBeRemovedFromTotal;
-		updatedCart.totalProductsPrice = updatedCart.totalProductsPrice - priceToBeDeductedFromTotal;
-
-		localStorage.setItem( 'woo-next-cart', JSON.stringify( updatedCart ) );
-		return updatedCart;
-
-	} else {
-		return existingCart;
-	}
+    localStorage.setItem('woo-next-cart', JSON.stringify(updatedCart));
+    return updatedCart;
+  } else {
+    return existingCart;
+  }
 };
 
 /**
  * Returns cart data in the required format.
  * @param {String} data Cart data
  */
-export const getFormattedCart = ( data ) => {
+export const getFormattedCart = (data) => {
+  let formattedCart = null;
 
-	let formattedCart = null;
+  if (undefined === data || !data.cart.contents.nodes.length) {
+    return formattedCart;
+  }
 
-	if ( undefined === data || ! data.cart.contents.nodes.length ) {
-		return formattedCart;
-	}
+  const givenProducts = data.cart.contents.nodes;
 
-	const givenProducts = data.cart.contents.nodes;
+  // Create an empty object.
+  formattedCart = {};
+  formattedCart.products = [];
+  let totalProductsCount = 0;
 
-	// Create an empty object.
-	formattedCart = {};
-	formattedCart.products = [];
-	let totalProductsCount = 0;
+  for (let i = 0; i < givenProducts.length; i++) {
+    const givenProduct = givenProducts[i].product;
+    const product = {};
+    const total = getFloatVal(givenProducts[i].total);
 
-	for( let i = 0; i < givenProducts.length; i++  ) {
-		const givenProduct = givenProducts[ i ].product;
-		const product = {};
-		const total = getFloatVal( givenProducts[ i ].total );
+    product.productId = givenProduct.productId;
+    product.cartKey = givenProducts[i].key;
+    product.name = givenProduct.name;
+    product.qty = givenProducts[i].quantity;
+    product.price = total / product.qty;
+    product.totalPrice = givenProducts[i].total;
+    product.image = {
+      sourceUrl: givenProduct.image.sourceUrl,
+      srcSet: givenProduct.image.srcSet,
+      title: givenProduct.image.title,
+    };
 
-		product.productId = givenProduct.productId;
-		product.cartKey = givenProducts[ i ].key;
-		product.name = givenProduct.name;
-		product.qty = givenProducts[ i ].quantity;
-		product.price = total / product.qty;
-		product.totalPrice = givenProducts[ i ].total;
-		product.image = {
-			sourceUrl: givenProduct.image.sourceUrl,
-			srcSet: givenProduct.image.srcSet,
-			title: givenProduct.image.title
-		};
+    totalProductsCount += givenProducts[i].quantity;
 
-		totalProductsCount += givenProducts[ i ].quantity;
+    // Push each item into the products array.
+    formattedCart.products.push(product);
+  }
 
-		// Push each item into the products array.
-		formattedCart.products.push( product );
-	}
+  formattedCart.totalProductsCount = totalProductsCount;
+  formattedCart.totalProductsPrice = data.cart.total;
 
-	formattedCart.totalProductsCount = totalProductsCount;
-	formattedCart.totalProductsPrice = data.cart.total;
-
-	return formattedCart;
-
+  return formattedCart;
 };
 
-export const createCheckoutData = ( order ) => {
-	const checkoutData = {
-		clientMutationId: v4(),
+export const createCheckoutData = (order) => {
+  const checkoutData = {
+    clientMutationId: v4(),
 
-		billing: {
-			firstName: order.firstName,
-			lastName: order.lastName,
-			address1: order.address1,
-			address2: order.address2,
-			city: order.city,
-			country: order.country,
-			state: order.state,
-			postcode: order.postcode,
-			email: order.email,
-			phone: order.phone,
-			company: order.company,
-		},
-		shipping: {
-			firstName: order.firstName,
-			lastName: order.lastName,
-			address1: order.address1,
-			address2: order.address2,
-			city: order.city,
-			country: order.country,
-			state: order.state,
-			postcode: order.postcode,
-			email: order.email,
-			phone: order.phone,
-			company: order.company,
-		},
-		shipToDifferentAddress: false,
-		paymentMethod: order.paymentMethod,
-		isPaid: false,
-		transactionId: "hjkhjkhsdsdiui"
-	};
+    billing: {
+      firstName: order.firstName,
+      lastName: order.lastName,
+      address1: order.address1,
+      address2: order.address2,
+      city: order.city,
+      country: order.country,
+      state: order.state,
+      postcode: order.postcode,
+      email: order.email,
+      phone: order.phone,
+      company: order.company,
+    },
+    shipping: {
+      firstName: order.firstName,
+      lastName: order.lastName,
+      address1: order.address1,
+      address2: order.address2,
+      city: order.city,
+      country: order.country,
+      state: order.state,
+      postcode: order.postcode,
+      email: order.email,
+      phone: order.phone,
+      company: order.company,
+    },
+    shipToDifferentAddress: false,
+    paymentMethod: order.paymentMethod,
+    isPaid: false,
+    transactionId: 'hjkhjkhsdsdiui',
+  };
 
-	return checkoutData;
+  return checkoutData;
 };
 
 /**
@@ -251,32 +275,28 @@ export const createCheckoutData = ( order ) => {
  * Creates an array in above format with the newQty (updated Qty ).
  *
  */
-export const getUpdatedItems = ( products, newQty, cartKey ) => {
+export const getUpdatedItems = (products, newQty, cartKey) => {
+  // Create an empty array.
+  const updatedItems = [];
 
-	// Create an empty array.
-	const updatedItems = [];
+  // Loop through the product array.
+  products.map((cartItem) => {
+    // If you find the cart key of the product user is trying to update, push the key and new qty.
+    if (cartItem.cartKey === cartKey) {
+      updatedItems.push({
+        key: cartItem.cartKey,
+        quantity: parseInt(newQty),
+      });
 
-	// Loop through the product array.
-	products.map( ( cartItem ) => {
+      // Otherwise just push the existing qty without updating.
+    } else {
+      updatedItems.push({
+        key: cartItem.cartKey,
+        quantity: cartItem.qty,
+      });
+    }
+  });
 
-		// If you find the cart key of the product user is trying to update, push the key and new qty.
-		if ( cartItem.cartKey === cartKey ) {
-
-			updatedItems.push( {
-				key: cartItem.cartKey,
-				quantity: parseInt( newQty )
-			} );
-
-			// Otherwise just push the existing qty without updating.
-		} else {
-			updatedItems.push( {
-				key: cartItem.cartKey,
-				quantity: cartItem.qty
-			} );
-		}
-	} );
-
-	// Return the updatedItems array with new Qtys.
-	return updatedItems;
-
+  // Return the updatedItems array with new Qtys.
+  return updatedItems;
 };
