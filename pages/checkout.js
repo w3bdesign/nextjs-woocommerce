797c6f877c8f2d87877cbaff9fed426131f37460
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-06 23:55:54 +0200

    Add checkout form page and component

---
+++ pages/checkout.js
@@ -0,0 +1,10 @@
+import CheckoutForm from '../components/checkout/CheckoutForm';
+
+const Checkout = () => (
+  <div className="container mt-5">
+    <h1 className="mt-5 mb-4">Bestilling</h1>
+    <CheckoutForm />
+  </div>
+);
+
+export default Checkout;
