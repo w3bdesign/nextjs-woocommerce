d6c7056fe56f4be10ac66a17511a85e72cdbcf02
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-06-18 01:37:39 +0200

    Add SVG X to cart items

--- components/Cart/CartPage/CartItem.component.jsx
+++ components/Cart/CartPage/CartItem.component.jsx
@@ -15,7 +15,29 @@ const CartItem = ({
   return (
     <tr className="bg-gray-100">
       <td className="px-4 py-2 border">
-        <SVGCloseX />
+        <span>
+        <svg
+          id="xsvg"
+          onClick={() => {
+            //setisExpanded(!isExpanded);
+          }}
+         
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="feather feather-x"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+          
+        </span>
       </td>
       <td className="px-4 py-2 border">
         <img
