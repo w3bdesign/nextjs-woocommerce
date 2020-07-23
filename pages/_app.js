14b40060ab9160a03af9da0fcfffd634bdc53d7a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-07-24 00:14:02 +0200

    Update paths

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,14 @@ import { AppProvider } from 'utils/context/AppContext';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
-import Footer from '../components/Footer/Footer.component';
+import Footer from 'components/Footer/Footer.component';
 
 import "styles/index.css";
 import 'styles/algolia.min.css';
 import 'styles/animate.min.css';
 import 'nprogress/nprogress.css';
 
-import client from '../utils/apollo/ApolloClient';
+import client from 'utils/apollo/ApolloClient';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
