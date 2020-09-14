a4577dc54bf4a4a9410983c853771236703cd6c0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-15 01:36:57 +0200

    Add w3bdesign to footer credits

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
       <footer className="container px-6 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
-        <div className="p-6">Copyright &copy; {new Date().getFullYear()} Daniel F aka w3bdesign</div>
+        <div className="p-6">Copyright &copy; {new Date().getFullYear()} Daniel / w3bdesign</div>
       </footer>
     </>
   );
