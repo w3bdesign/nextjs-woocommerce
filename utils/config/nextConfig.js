6db539d42d208c568fe29e8bc06247b8cd4bc5d6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-14 18:40:00 +0200

    Change url to production details

--- utils/config/nextConfig.js
+++ utils/config/nextConfig.js
@@ -2,18 +2,19 @@
 
 // Live details
 
-/*
+
 export const WOO_CONFIG = {
-  GRAPHQL_URL: 'https://woocommerce.dfweb.no/graphql',
+  GRAPHQL_URL: 'https://woo.dfweb.no/graphql',
   ALGOLIA_APP_ID: '7L9M5Y0B1K',
   ALGOLIA_PUBLIC_API_KEY: '925cc92373120f1bf477bb8ce0e71649',
   ALGOLIA_INDEX_NAME: 'dfweb',
   PLACEHOLDER_IMAGE_URL: "https://via.placeholder.com/272",
 };
-*/
+
 
 // Development details
 
+/*
 export const WOO_CONFIG = {
   GRAPHQL_URL: 'http://localhost/wp/graphql',
   ALGOLIA_APP_ID: '7L9M5Y0B1K',
@@ -22,5 +23,6 @@ export const WOO_CONFIG = {
   PLACEHOLDER_SMALL_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1595540954/Nextjs-Woocommerce/placeholder-272-272_pg8t0z.jpg',
   PLACEHOLDER_LARGE_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1595541416/Nextjs-Woocommerce/placeholder-616-616_beecp5.jpg',
 };
+*/
 
 export default WOO_CONFIG;
