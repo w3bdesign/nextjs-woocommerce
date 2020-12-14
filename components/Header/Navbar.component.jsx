c84bdccace8faafc13281079c86bb60a471687b9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-12-14 06:20:55 +0100

    Increase font size of navbar menu items

--- components/Header/Navbar.component.jsx
+++ components/Header/Navbar.component.jsx
@@ -22,14 +22,14 @@ const Navbar = () => {
             <ul className="items-center justify-between pt-4 text-base text-gray-700 md:flex md:pt-0">
               <li>
                 <Link href="/produkter">
-                  <a className="inline-block py-2 pr-4 no-underline hover:text-black hover:underline">
+                  <a className="inline-block py-2 pr-4 text-xl font-bold no-underline hover:underline">
                     Produkter
                   </a>
                 </Link>
               </li>
               <li>
                 <Link href="/kategorier">
-                  <a className="inline-block py-2 pr-4 no-underline hover:text-black hover:underline">
+                  <a className="inline-block py-2 pr-4 text-xl font-bold no-underline hover:underline">
                     Kategorier
                   </a>
                 </Link>
