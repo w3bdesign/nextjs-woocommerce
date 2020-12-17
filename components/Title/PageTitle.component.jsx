fb991c85d542f70e0106792da373148b46f2b663
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-12-17 04:43:15 +0100

    Modify README and increase pagetitle margin

--- components/Title/PageTitle.component.jsx
+++ components/Title/PageTitle.component.jsx
@@ -2,13 +2,13 @@ const PageTitle = ({ title, marginleft }) => {
   return (
     <>
       {marginleft ? (
-        <section className="container pl-8 mx-auto mt-24 text-center bg-white">
+        <section className="container pl-8 mx-auto mt-32 text-center bg-white">
           <span className="py-2 text-xl font-bold tracking-wide text-center text-gray-800 no-underline uppercase hover:no-underline">
             {title}
           </span>
         </section>
       ) : (
-        <section className="container pl-4 mx-auto mt-24 text-center bg-white">
+        <section className="container pl-4 mx-auto mt-32 text-center bg-white">
           <span className="py-2 text-xl font-bold tracking-wide text-center text-gray-800 no-underline uppercase hover:no-underline">
             {title}
           </span>
--- utils/config/nextConfig.js
+++ utils/config/nextConfig.js
@@ -4,11 +4,11 @@
 
 export const WOO_CONFIG = {
   GRAPHQL_URL: 'https://woo.dfweb.no/graphql',
-  ALGOLIA_APP_ID: '7L9M5Y0B1K',
-  ALGOLIA_PUBLIC_API_KEY: '6f03421b21d2320c3d544777235debf1',
   ALGOLIA_INDEX_NAME: 'dfweb',
-  PLACEHOLDER_SMALL_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1600187495/Nextjs-Woocommerce/337_utsb7h.jpg',
-  PLACEHOLDER_LARGE_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1595541416/Nextjs-Woocommerce/placeholder-616-616_beecp5.jpg',
+  PLACEHOLDER_SMALL_IMAGE_URL:
+    'https://res.cloudinary.com/duelisue0/image/upload/v1600187495/Nextjs-Woocommerce/337_utsb7h.jpg',
+  PLACEHOLDER_LARGE_IMAGE_URL:
+    'https://res.cloudinary.com/duelisue0/image/upload/v1595541416/Nextjs-Woocommerce/placeholder-616-616_beecp5.jpg',
 };
 
 // Development details
@@ -16,8 +16,6 @@ export const WOO_CONFIG = {
 /*
 export const WOO_CONFIG = {
   GRAPHQL_URL: 'http://localhost/wp/graphql',
-  ALGOLIA_APP_ID: '7L9M5Y0B1K',
-  ALGOLIA_PUBLIC_API_KEY: '925cc92373120f1bf477bb8ce0e71649',
   ALGOLIA_INDEX_NAME: 'dfweb',
   PLACEHOLDER_SMALL_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1595540954/Nextjs-Woocommerce/placeholder-272-272_pg8t0z.jpg',
   PLACEHOLDER_LARGE_IMAGE_URL: 'https://res.cloudinary.com/duelisue0/image/upload/v1595541416/Nextjs-Woocommerce/placeholder-616-616_beecp5.jpg',
