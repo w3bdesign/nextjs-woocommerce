0262ce5009d527e7f0a98a0f2160ce43d9d9c3f8
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-04-05 20:23:45 +0200

    Fix bug with React hook form upgrade

--- components/Checkout/Billing.component.jsx
+++ components/Checkout/Billing.component.jsx
@@ -3,45 +3,60 @@ import { useForm } from 'react-hook-form';
 const Input = ({
   name,
   label,
-  register,
+  //register,
+  registerRef,
   placeholder,
   value,
   parameters,
-  type = 'text',  
+  type = 'text',
   readOnly = false,
-}) => (
-  <>
-    <label className="pb-4">{label}</label>
-    <input
-      className="w-full px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black"
-      name={name}
-      placeholder={placeholder}
-      type="text"
-      value={value}
-      ref={register(parameters)}
-      type={type}      
-      readOnly={readOnly}
-    />
-  </>
-);
+}) => {
+  const { register } = useForm();
+
+  return (
+    <>
+      <label className="pb-4">{label}</label>
+      <input
+        className="w-full px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black"
+        name={name}
+        placeholder={placeholder}
+        type="text"
+        value={value}
+        {...register(registerRef, parameters)}
+        type={type}
+        readOnly={readOnly}
+      />
+    </>
+  );
+};
 
 const Billing = ({ onSubmit }) => {
-  const { register, handleSubmit, errors } = useForm();
+  const inputClasses =
+    'w-full px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black';
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <>
       <section className="text-gray-700">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="container p-4 py-2 mx-auto">           
+          <div className="container p-4 py-2 mx-auto">
             <div className="mx-auto lg:w-1/2 md:w-2/3">
               <div className="flex flex-wrap -m-2">
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Fornavn</label>
+                  <input
+                    className={inputClasses}
                     name="firstName"
                     placeholder="Fornavn"
                     label="Fornavn"
-                    register={register}
-                    parameters={{ required: 'Dette feltet er påkrevd' }}
+                    type="text"
+                    {...register('firstName', {
+                      required: 'Dette feltet er påkrevd',
+                    })}
                   />
                   {errors.firstName && (
                     <span className="text-red-500">
@@ -50,12 +65,16 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Etternavn</label>
+                  <input
+                    className={inputClasses}
                     name="lastName"
                     placeholder="Etternavn"
                     label="Etternavn"
-                    register={register}
-                    parameters={{ required: 'Dette feltet er påkrevd' }}
+                    type="text"
+                    {...register('lastName', {
+                      required: 'Dette feltet er påkrevd',
+                    })}
                   />
                   {errors.lastName && (
                     <span className="text-red-500">
@@ -64,13 +83,18 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Addresse</label>
+                  <input
+                    className={inputClasses}
                     name="address1"
                     placeholder="Adresse"
                     label="Adresse"
-                    register={register}
-                    parameters={{ required: 'Dette feltet er påkrevd' }}
+                    type="text"
+                    {...register('address1', {
+                      required: 'Dette feltet er påkrevd',
+                    })}
                   />
+
                   {errors.address1 && (
                     <span className="text-red-500">
                       FEIL: {errors.address1.message}
@@ -78,12 +102,14 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Addresse</label>
+                  <input
+                    className={inputClasses}
                     name="postcode"
                     placeholder="Postnummer"
                     label="Postnummer"
-                    register={register}
-                    parameters={{
+                    type="text"
+                    {...register('postcode', {
                       required: 'Dette feltet er påkrevd',
                       minLength: {
                         value: 4,
@@ -97,7 +123,7 @@ const Billing = ({ onSubmit }) => {
                         value: /^[0-9]+$/i,
                         message: 'Postnummer må bare være tall',
                       },
-                    }}
+                    })}
                   />
                   {errors.postcode && (
                     <span className="text-red-500">
@@ -106,13 +132,18 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Sted</label>
+                  <input
+                    className={inputClasses}
                     name="city"
                     placeholder="Sted"
                     label="Sted"
-                    register={register}
-                    parameters={{ required: 'Dette feltet er påkrevd' }}
+                    type="text"
+                    {...register('city', {
+                      required: 'Dette feltet er påkrevd',
+                    })}
                   />
+
                   {errors.city && (
                     <span className="text-red-500">
                       FEIL: {errors.city.message}
@@ -120,19 +151,22 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Epost</label>
+                  <input
+                    className={inputClasses}
                     name="email"
                     placeholder="Epost"
                     label="Epost"
-                    register={register}
-                    parameters={{
+                    type="text"
+                    {...register('email', {
                       required: 'Dette feltet er påkrevd',
                       pattern: {
                         value: /[^@]+@[^@]+\.[^@]+/i,
                         message: 'Du må oppgi en gyldig epost',
                       },
-                    }}
+                    })}
                   />
+
                   {errors.email && (
                     <span className="text-red-500">
                       FEIL: {errors.email.message}
@@ -140,12 +174,14 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <label className="pb-4">Telefon</label>
+                  <input
+                    className={inputClasses}
                     name="phone"
                     placeholder="Telefon"
                     label="Telefon"
-                    register={register}
-                    parameters={{
+                    type="text"
+                    {...register('phone', {
                       required: 'Dette feltet er påkrevd',
                       minLength: {
                         value: 8,
@@ -160,8 +196,9 @@ const Billing = ({ onSubmit }) => {
                         value: /^[0-9]+$/i,
                         message: 'Ikke gyldig telefonnummer',
                       },
-                    }}
+                    })}
                   />
+
                   {errors.phone && (
                     <span className="text-red-500">
                       FEIL: {errors.phone.message}
@@ -169,15 +206,15 @@ const Billing = ({ onSubmit }) => {
                   )}
                 </div>
                 <div className="w-1/2 p-2">
-                  <Input
+                  <input
                     name="paymentMethod"
                     placeholder="paymentMethod"
                     label=""
                     type="hidden"
                     value="cod"
-                    register={register}
                     checked
-                    readOnly
+                    type="hidden"
+                    {...register('paymentMethod')}
                   />
                 </div>
                 <div className="w-full p-2">
