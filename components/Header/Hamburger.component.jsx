f134aea387e9005de4136926d9f1328d775c6d80
Claudia Bressi <claudia.bressi@bitrock.it>
2021-10-24 15:39:49 +0200

    spacing

--- components/Header/Hamburger.component.jsx
+++ components/Header/Hamburger.component.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
 import Menu from './Menu.component';
-
 /**
  * Shows the mobile menu.
  * Shows a X when mobile menu is expanded.
