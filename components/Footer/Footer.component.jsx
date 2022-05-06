eeb672c6f8965debc87d7912a591a1da005987a6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-06 03:55:25 +0200

    Fix footer

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -1,8 +1,8 @@
 /**
  * Footer of the application.
  */
-const Footer = () => (
-  <footer className="container px-6 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
+ const Footer = () => (
+  <footer className="w-full sm:absolute sm:bottom-0 px-6 mb-28 sm:mb-0 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
     <div className="p-6">
       Copyright &copy; {new Date().getFullYear()} Daniel / w3bdesign
     </div>
