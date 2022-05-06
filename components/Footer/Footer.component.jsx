d61dbc10a05d1f989f0cd30a7447dffa2acc34a2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-05-06 04:52:26 +0200

    Version 1.0.25

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -1,7 +1,7 @@
 /**
  * Footer of the application.
  */
- const Footer = () => (
+const Footer = () => (
   <footer className="w-full sm:absolute sm:bottom-0 px-6 mb-28 sm:mb-0 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
     <div className="p-6">
       Copyright &copy; {new Date().getFullYear()} Daniel / w3bdesign
