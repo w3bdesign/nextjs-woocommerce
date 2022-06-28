b50d3caf872be8356c73f31eee781f64addb000f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-06-28 02:22:53 +0200

    Update AppContext.js

--- utils/context/AppContext.js
+++ utils/context/AppContext.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 
-export const AppContext = React.createContext();
+export const AppContext = createContext();
 
 /**
  * Provides a global application context for the entire application with the cart contents
@@ -13,12 +13,11 @@ export const AppProvider = ({ children }) => {
     // Check if we are client-side before we access the localStorage
     if (process.browser) {
       let cartData = localStorage.getItem('woocommerce-cart');
-
       cartData = null !== cartData ? JSON.parse(cartData) : '';
-
       setCart(cartData);
     }
   }, []);
+  
   return (
     <AppContext.Provider value={[cart, setCart]}>
       {children}
