6e015f4d2f3bd34e41dd0250dc700e994cebe61f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-07-26 01:24:38 +0200

    Add footer margin

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -2,7 +2,7 @@
  * Footer of the application.
  */
 const Footer = () => (
-  <footer className="w-full sm:mt-20 mt-2 sm:bottom-0 px-6 mb-28 sm:mb-2 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
+  <footer className="w-full md:w-11/12 sm:mt-20 mt-2 sm:bottom-0 px-6 mb-28 sm:mb-2 mx-auto text-center bg-white border border-gray-300 rounded-lg shadow">
     <div className="p-6">
       Copyright &copy; {new Date().getFullYear()} Daniel / w3bdesign
     </div>
