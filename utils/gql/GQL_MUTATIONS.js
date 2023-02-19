464e75c6e3e60cbc51e5350cec8fcbc21d31f355
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-19 03:28:31 +0100

    Remove product from cart

--- utils/gql/GQL_MUTATIONS.js
+++ utils/gql/GQL_MUTATIONS.js
@@ -77,52 +77,58 @@ export const UPDATE_CART = gql`
       items {
         key
         product {
-          id
-          databaseId
-          name
-          description
-          type
-          onSale
-          slug
-          averageRating
-          reviewCount
-          image {
+          node {
             id
-            sourceUrl
-            altText
-          }
-          galleryImages {
-            nodes {
+            databaseId
+            name
+            description
+            type
+            onSale
+            slug
+            averageRating
+            reviewCount
+            image {
               id
               sourceUrl
               altText
             }
-          }
-        }
-        variation {
-          id
-          databaseId
-          name
-          description
-          type
-          onSale
-          price
-          regularPrice
-          salePrice
-          image {
-            id
-            sourceUrl
-            altText
-          }
-          attributes {
-            nodes {
-              id
-              attributeId
-              name
-              value
+            galleryImages {
+              nodes {
+                id
+                sourceUrl
+                altText
+              }
             }
           }
         }
+
+        variation {
+          node {
+            id
+            databaseId
+            name
+            description
+            type
+            onSale
+            price
+            regularPrice
+            salePrice
+            image {
+              id
+              sourceUrl
+              altText
+            }
+            attributes {
+              nodes {
+                id
+                attributeId
+                name
+                value
+              }
+            }
+          }
+        }
+
         quantity
         total
         subtotal
@@ -131,23 +137,34 @@ export const UPDATE_CART = gql`
       removed {
         key
         product {
-          id
-          databaseId
+          node {
+            id
+            databaseId
+          }
         }
+
         variation {
-          id
-          databaseId
+          node {
+            id
+            databaseId
+          }
         }
       }
+
       updated {
         key
         product {
-          id
-          databaseId
+          node {
+            id
+            databaseId
+          }
         }
+
         variation {
-          id
-          databaseId
+          node {
+            id
+            databaseId
+          }
         }
       }
     }
