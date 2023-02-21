5d3aa5c4c5e37157127e1c14588920f766fe6248
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-21 01:27:32 +0100

    Language

--- refactor/src/pages/_document.tsx
+++ refactor/src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="nb-NO">
       <Head />
       <body>
         <Main />
