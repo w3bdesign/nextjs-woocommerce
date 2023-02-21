e9dafd7787f575ec59a59beab3360ba01fcc8f59
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-21 01:27:39 +0100

    Add to cart

---
+++ refactor/src/components/Product/AddToCart.component.tsx
@@ -0,0 +1,71 @@
+// Imports
+import { useContext, useState } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { v4 as uuidv4 } from 'uuid';
+
+// Components
+import Button from '@/components/UI/Button.component';
+
+// State
+import { CartContext } from '@/utils/context/CartProvider';
+
+// GraphQL
+import { GET_CART } from '@/utils/gql/GQL_QUERIES';
+import { ADD_TO_CART } from '@/utils/gql/GQL_MUTATIONS';
+
+/**
+ * Handles the Add to cart functionality.
+ * Uses GraphQL for product data
+ * @param {Object} product // Product data
+ */
+const AddToCart = ({ product }: any) => {
+  const [, setCart] = useContext<any>(CartContext);
+  const [requestError, setRequestError] = useState<boolean>(false);
+
+  const productId = product.databaseId ? product.databaseId : product;
+
+  const productQueryInput = {
+    clientMutationId: uuidv4(), // Generate a unique id.
+    productId,
+  };
+
+  const [addToCart, { loading: addToCartLoading, error: addToCartError }] =
+    useMutation(ADD_TO_CART, {
+      variables: {
+        input: productQueryInput,
+      },
+      onCompleted: () => {
+        // Update the cart with new values in React context.
+        // refetch();
+        // If error.
+
+        console.log('Added to cart ...', product);
+
+        if (addToCartError) {
+          setRequestError(true);
+        }
+      },
+      onError: () => {
+        setRequestError(true);
+      },
+    });
+
+  const handleAddToCart = () => {
+    console.log('Tried to add product ', product);
+    setCart(product);
+    addToCart();
+  };
+
+  return (
+    <>
+      <Button
+        handleButtonClick={() => handleAddToCart()}
+        buttonDisabled={addToCartLoading || requestError}
+      >
+        KJØP
+      </Button>
+    </>
+  );
+};
+
+export default AddToCart;
