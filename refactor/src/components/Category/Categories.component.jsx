26edfe549e925765e0a1ee193f3036c71073b8b2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-22 06:00:08 +0100

    Start work on kategorier

---
+++ refactor/src/components/Category/Categories.component.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
+
+/**
+ * Map over the categories and display them individually.
+ * Uses uuidv4 for unique key IDs
+ * @param {Object} categories Product categories
+ */
+const Categories = ({ categories }) => (
+  <section className="container mx-auto bg-white">
+    <div className="grid gap-2 px-2 pt-2 pb-2 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 xs:grid-cols-3">
+      {categories.map(({ id, name, slug }) => (
+        <Link
+          key={uuidv4()}
+          as={`/kategori/${slug}?id=${id}`}
+          href="/kategori/[id]"
+          passHref
+        >
+          <div className="p-6 cursor-pointer">
+            <div className="flex items-center justify-center w-full h-16 text-center border border-gray-300 rounded-lg shadow hover:shadow-outline">
+              <p className="text-lg">{name}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  </section>
+);
+
+export default Categories;
---
+++ refactor/src/pages/kategorier.js
@@ -0,0 +1,27 @@
+import Categories from '@/components/Category/Categories.component';
+
+import client from '@/utils/apollo/ApolloClient.js';
+
+import { FETCH_ALL_CATEGORIES_QUERY } from '@/utils/gql/GQL_QUERIES';
+
+/**
+ * Category page displays all of the categories
+ */
+const CategoryPage = ({ categories }) => (
+  <div>{categories && <Categories categories={categories} />}</div>
+);
+
+export default CategoryPage;
+
+export async function getStaticProps() {
+  const result = await client.query({
+    query: FETCH_ALL_CATEGORIES_QUERY,
+  });
+
+  return {
+    props: {
+      categories: result.data.productCategories.nodes,
+    },
+    revalidate: 10,
+  };
+}
