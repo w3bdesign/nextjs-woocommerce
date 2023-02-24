5d99843538f39a62d97107a6765696800f3e9f1f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-24 04:12:28 +0100

    Update input styling

--- refactor/src/components/Cart/CartContents.component.tsx
+++ refactor/src/components/Cart/CartContents.component.tsx
@@ -93,7 +93,7 @@ const CartContents = () => {
                 </span>
                 <span className="inline-block mt-4 w-20 h-12 md:w-full lg:w-full xl:w-full">
                   <input
-                    className="w-12"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     type="number"
                     min="1"
                     value={item.qty}
--- refactor/src/components/Input/InputField.component.tsx
+++ refactor/src/components/Input/InputField.component.tsx
@@ -23,7 +23,7 @@ export const InputField = ({
       {label}
     </label>
     <input
-      className="w-full px-4 py-2 mt-2 text-base bg-white border border-gray-400 rounded focus:outline-none focus:border-black"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       name={name}
       id={name}
       placeholder={label}
