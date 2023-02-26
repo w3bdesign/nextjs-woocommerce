cc9671bd2ce164a9b612243eb627b2d2f4e2791f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-26 08:22:12 +0100

    Categories Typescript

--- refactor/src/components/Category/Categories.component.tsx
+++ refactor/src/components/Category/Categories.component.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link';
 import { v4 as uuidv4 } from 'uuid';
 
-/**
- * Map over the categories and display them individually.
- * Uses uuidv4 for unique key IDs
- * @param {Object} categories Product categories
- */
-const Categories = ({ categories }: any) => (
+interface ICategoriesProps {
+  categories: { id: string; name: string; slug: string }[];
+}
+
+const Categories = ({ categories }: ICategoriesProps) => (
   <section className="container mx-auto bg-white">
     <div className="grid gap-2 px-2 pt-2 pb-2 lg:px-0 xl:px-0 md:px-0 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 xs:grid-cols-3">
-      {categories.map(({ id, name, slug }: any) => (
+      {categories.map(({ id, name, slug }) => (
         <Link
           key={uuidv4()}
           as={`/kategori/${slug}?id=${id}`}
