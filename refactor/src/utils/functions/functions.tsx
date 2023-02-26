4b541d6c8869cbe3a35837ce6563e057119f117b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-26 06:33:14 +0100

    Refactor

--- refactor/src/utils/functions/functions.tsx
+++ refactor/src/utils/functions/functions.tsx
@@ -130,14 +130,7 @@ export const getCustomNumberValidation = ({
     pattern: { value: patternValue, message: pattern },
   };
 
-  // TODO Fix this Typescript error
-  return Object.keys(validationObj).reduce((acc, key) => {
-    if (validationObj[key].value) {
-      acc[key] = validationObj[key];
-    }
-
-    return acc;
-  }, {});
+  return validationObj;
 };
 
 /**
