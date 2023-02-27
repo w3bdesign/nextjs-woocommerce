5d5437bfc6236f249347974c635fe42d54d3147e
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-02-27 03:17:08 +0100

    Cleanup

--- refactor/src/components/UI/Button.component.tsx
+++ refactor/src/components/UI/Button.component.tsx
@@ -28,12 +28,11 @@ const Button = ({
     onClick={handleButtonClick}
     disabled={buttonDisabled}
     className={`px-2 lg:px-4 py-2 font-bold bg-blue-500 border border-gray-400 border-solid rounded text-white ease-in-out transition-all duration-300 disabled:opacity-50
-    
-    ${
-      color === 'blue'
-        ? 'bg-blue-500 hover:bg-blue-600'
-        : 'bg-red-500 hover:bg-red-600'
-    }
+      ${
+        color === 'blue'
+          ? 'bg-blue-500 hover:bg-blue-600'
+          : 'bg-red-500 hover:bg-red-600'
+      }
     `}
   >
     {children}
