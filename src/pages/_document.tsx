062070d1192f4743705e25cab6a49c3a14b92da1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-26 00:34:19 +0200

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="nb-NO">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
