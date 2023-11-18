5b4e9bd229951703d54a806191c7f3640580f842
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-18 03:02:12 +0100

    Update ApolloClient.js

--- src/utils/apollo/ApolloClient.js
+++ src/utils/apollo/ApolloClient.js
@@ -9,6 +9,57 @@ import {
 
 const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
 
+
+
+function clearStorageAndCookies() {
+  if (process.browser) {
+    // Clear localStorage
+    localStorage.clear();
+
+    // Clear cookies
+    document.cookie.split(";").forEach((c) => {
+      document.cookie = c
+        .replace(/^ +/, "")
+        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
+  }
+}
+
+if (process.browser) {
+  window.addEventListener("error", (event) => {
+    console.error("Application crashed. Clearing localStorage and cookies.");
+    clearStorageAndCookies();
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Middleware operation
  * If we have a session token in localStorage, add it to the GraphQL request as a Session header.
