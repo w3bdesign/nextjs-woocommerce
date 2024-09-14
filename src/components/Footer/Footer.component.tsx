672565cbd9d53442a6c49022855e301d6747b8c6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-09-14 03:27:32 +0200

    Update Footer.component.tsx

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -6,10 +6,10 @@ import React from 'react';
  * @returns {JSX.Element} - Rendered component
  */
 const Footer = () => (
-  <footer className="w-full bg-white">
+  <footer className="w-full bg-white border-t border-gray-200">
     <div className="container flex items-center justify-between px-6 py-3 mx-auto mt-0 md:min-w-96">
-      <div className="text-gray-700">
-        Copyright &copy; {new Date().getFullYear()} Daniel / w3bdesign
+      <div className="text-gray-600">
+        &copy; {new Date().getFullYear()} Daniel / w3bdesign
       </div>
     </div>
   </footer>
