df1307c067911cf4230510b2ff075ba2d997a0fb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-09-15 04:46:35 +0200

    Update Layout.component.tsx

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -56,7 +56,7 @@ const Layout = ({ children, title }: ILayoutProps) => {
   }, [refetch]);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen min-w-[550px]">
       <Header title={title} />
       <PageTitle title={title} />
       <main className="flex-grow">
