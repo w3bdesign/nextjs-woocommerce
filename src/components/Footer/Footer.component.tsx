6425ec0a4cc13cc624715b174454981ed30190df
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-09-24 01:32:27 +0200

    Update Footer.component.tsx

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -5,7 +5,7 @@
  */
 const Footer = () => (
   <footer className="bg-white mb-4 hidden md:block px-4">
-    <div className="container flex md:flex-wrap flex-col md:flex-row items-center justify-between px-6 py-3 mx-auto mt-0 md:min-w-96 border border-gray-200 rounded">
+    <div className="container flex md:flex-wrap flex-col md:flex-row items-center justify-between px-6 py-3 mx-auto mt-0 md:min-w-96 border border-gray-200 rounded lg:max-w-[1490px]">
       <div className="text-gray-600 mx-auto">
         &copy; {new Date().getFullYear()} Daniel / w3bdesign
       </div>
