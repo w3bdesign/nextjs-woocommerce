d2d9bbf5a59cd51af3145ceb73f74502132041d6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-09-23 22:56:45 +0200

    Update Layout.component.tsx

--- src/components/Layout/Layout.component.tsx
+++ src/components/Layout/Layout.component.tsx
@@ -57,7 +57,7 @@ const Layout = ({ children, title }: ILayoutProps) => {
 
   return (
     <div className="flex flex-col min-h-screen w-full mx-auto">
-      <div className="container min-w-[140vw] sm:min-w-[100vw] md:px-4 lg:px-6 py-2 lg:max-w-[1600px] mx-auto">
+      <div className="container w-full px-2 sm:px-4 md:px-6 lg:px-8 py-2 mx-auto max-w-[100vw] sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1320px]">
         <Header title={title} />
         <PageTitle title={title} />
         <main className="flex-grow">
