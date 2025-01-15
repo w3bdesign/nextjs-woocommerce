779db89580a8f754b7408fdd1b021818470d98cd
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-01-16 00:03:49 +0100

    Remove unused variable

--- src/components/Layout/PageTitle.component.tsx
+++ src/components/Layout/PageTitle.component.tsx
@@ -1,6 +1,5 @@
 interface IPageTitleProps {
   title: string;
-  marginLeft?: boolean;
 }
 
 /**
@@ -10,7 +9,7 @@ interface IPageTitleProps {
  * @param {string} title - Title for the page. Is set in <title>{title}</title>
  * @returns {JSX.Element} - Rendered component
  */
-const PageTitle = ({ title, marginLeft }: IPageTitleProps) => (
+const PageTitle = ({ title }: IPageTitleProps) => (
   <section className="w-full bg-white border-b border-gray-200">
     <div className="container mx-auto py-12 px-6">
       <h1 className="text-2xl text-center tracking-wider text-gray-900 uppercase">
