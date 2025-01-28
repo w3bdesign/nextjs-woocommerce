1c5c17aa4b9c2cacd1a2c5ab8a5953ee4b79ddcb
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-01-28 03:38:22 +0100

    Typescript fix

--- src/components/Product/ProductFilters.component.tsx
+++ src/components/Product/ProductFilters.component.tsx
@@ -36,7 +36,7 @@ const ProductFilters = ({
           ) || [],
       ),
     ),
-  ).sort((a, b) => a.localeCompare(b)) as string[];
+  ).sort((a, b) => a.localeCompare(b));
 
   // Get unique colors from all products
   const availableColors = products
