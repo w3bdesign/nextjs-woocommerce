3906c02aabea99fd3a343a67633c2468d65e3f65
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2025-02-23 06:16:22 +0100

    Prettier

--- src/components/Index/Hero.component.tsx
+++ src/components/Index/Hero.component.tsx
@@ -19,16 +19,13 @@ const Hero = () => (
       />
       <div className="absolute inset-0 bg-black bg-opacity-30" />
     </div>
-    
+
     <div className="relative h-full container mx-auto flex items-center p-4 md:p-0">
       <div className="max-w-xl">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-light text-white mb-6">
           Stripete Zig Zag Pute Sett
         </h1>
-        <Button 
-          href="/produkter"
-          variant="hero"
-        >
+        <Button href="/produkter" variant="hero">
           Se Utvalget
         </Button>
       </div>
